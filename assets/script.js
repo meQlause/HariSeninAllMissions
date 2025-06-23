4c8cc10849577b1bb5aef00ea6a9fbe914697755
meQlause <me@ardial.tech>
2025-06-24 00:26:56 +0700

    feat: create initial login page layout.

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+function togglePassword() {
+  const input = document.querySelector('.password-wrapper input');
+  const icon = document.getElementById('toggle-icon');
+  
+  if (input.type === "password") {
+    input.type = "text";
+    icon.src = "assets/pictures/mdi_eye-on.png";
+    icon.alt = "Hide password";
+  } else {
+    input.type = "password";
+    icon.src = "assets/pictures/mdi_eye-off.png";
+    icon.alt = "Show password";
+  }
+}
