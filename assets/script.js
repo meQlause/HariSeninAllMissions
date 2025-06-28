a9dfc60748c3fdced31b7873a4c69f9ac2eedf19
meQlause <me@ardial.tech>
2025-06-28 14:35:56 +0700

    refactor: enhance task management functionality and improve sidebar interactions

--- assets/script.js
+++ assets/script.js
@@ -1,54 +1,87 @@
+const tasks = [
+  {
+    title: "Fix broken links",
+    description: "Audit and repair all 404 errors on site",
+    priority: "High",
+    due: "2025-06-26T14:00:00",
+    category: "Development",
+    isCompleted: false,
+  },
+  {
+    title: "Review project proposals",
+    description:
+      "Review all submitted project proposals and provide feedback by end of day",
+    priority: "High",
+    due: "2025-06-28T18:00:00",
+    category: "Work",
+    isCompleted: false,
+  },
+  {
+    title: "Team meeting",
+    description: "Weekly sync-up with the product team",
+    priority: "Medium",
+    due: "2025-06-30T09:00:00",
+    category: "Meetings",
+    isCompleted: false,
+  },
+  {
+    title: "Clean up workspace",
+    description: "Organize desk and delete unnecessary files",
+    priority: "Low",
+    due: "2025-07-03T15:00:00",
+    category: "Personal",
+    isCompleted: false,
+  },
+  {
+    title: "Submit report",
+    description: "Submit Q2 financial report",
+    priority: "High",
+    due: "2025-07-10T12:00:00",
+    category: "Finance",
+    isCompleted: true,
+  },
+];
+
 document.addEventListener("DOMContentLoaded", function () {
-  handleSidebarClick("AllItem");
+  const sidebarOrder = [
+    "allItem",
+    "important",
+    "today",
+    "upcoming",
+    "completed",
+    "overdue",
+  ];
+  document.querySelectorAll(".sidebar-item").forEach((item, index) => {
+    const counts = getNumbersOf(sidebarOrder[index]);
+    console.log(counts);
+    item.querySelector(".sidebar-number").innerHTML = counts;
+    if (counts === 0) {
+      console.log(counts);
+      item.classList.add("opacity-50");
+      item.classList.add("pointer-events-none");
+      item.classList.add("cursor-not-allowed");
+    }
+  });
+
+  tasks.forEach((item) => {
+    if (getDueLabel(item.due) === "Overdue") {
+      item.isCompleted = true;
+    }
+  });
+
+  handleSidebarClick("allItem");
 });
 
 function handleSidebarClick(filter) {
-  const tasks = [
-    {
-      title: "Fix broken links",
-      description: "Audit and repair all 404 errors on site",
-      priority: "High",
-      due: "2025-06-26T14:00:00",
-      category: "Development",
-    },
-    {
-      title: "Review project proposals",
-      description:
-        "Review all submitted project proposals and provide feedback by end of day",
-      priority: "High",
-      due: "2025-06-28T18:00:00",
-      category: "Work",
-    },
-    {
-      title: "Team meeting",
-      description: "Weekly sync-up with the product team",
-      priority: "Medium",
-      due: "2025-06-30T09:00:00",
-      category: "Meetings",
-    },
-    {
-      title: "Clean up workspace",
-      description: "Organize desk and delete unnecessary files",
-      priority: "Low",
-      due: "2025-07-03T15:00:00",
-      category: "Personal",
-    },
-    {
-      title: "Submit report",
-      description: "Submit Q2 financial report",
-      priority: "High",
-      due: "2025-07-10T12:00:00",
-      category: "Finance",
-    },
-  ];
+  removeActiveSidebar();
 
   const taskContainer = document.querySelector(".task-container");
+  taskContainer.innerHTML = "";
 
-  tasks.forEach((task) => {
+  const filteredList = filterList(tasks, filter);
+  filteredList.forEach((task) => {
     const div = document.createElement("div");
     div.className = "task-card";
-
-    const priority = `priority-${task.priority.toLocaleLowerCase()}`;
 
     div.innerHTML = `
             <div class="flex items-start">
@@ -58,9 +91,12 @@ function handleSidebarClick(filter) {
                         <h3 class="text-lg font-medium text-gray-800">
                         ${task.title}
                         </h3>
-                        <span class="${priority} text-xs px-2 py-1 rounded-full font-medium border">
-                        ${task.priority}
-                        </span>
+                        <div class="flex items-center">
+                            <span class="priority-${task.priority.toLocaleLowerCase()} text-xs px-2 py-1 rounded-full font-medium border">
+                            ${task.priority}
+                            </span>
+                            ${generateBadge(task)}
+                        </div>
                     </div>
                     <p class="text-gray-600 mt-1">${task.description}</p>
                     <div class="flex items-center mt-3 text-sm text-gray-500">
@@ -79,6 +115,62 @@ function handleSidebarClick(filter) {
       `;
     taskContainer.appendChild(div);
   });
+}
+
+function getNumbersOf(filter) {
+  return filterList(tasks, filter).length;
+}
+
+function removeActiveSidebar() {
+  document.querySelectorAll(".sidebar-item").forEach((item) => {
+    item.addEventListener("click", () => {
+      document
+        .querySelectorAll(".sidebar-item")
+        .forEach((i) => i.classList.remove("active"));
+      item.classList.add("active");
+    });
+  });
+}
+
+function filterList(tasks, filter) {
+  const activeTask = tasks.filter((item) => item.isCompleted === false);
+  switch (filter) {
+    case "allItem":
+      return tasks;
+    case "completed":
+      return tasks.filter(
+        (item) =>
+          item.isCompleted === true && getDueLabel(item.due) !== "Overdue"
+      );
+    case "overdue":
+      return tasks.filter((item) => getDueLabel(item.due) === "Overdue");
+    case "important":
+      return activeTask.filter((item) => item.priority === "High");
+    case "today":
+      return activeTask.filter((item) => getDueLabel(item.due) === "Due Today");
+    case "upcoming":
+      return activeTask.filter((item) => getDueLabel(item.due) === "Upcoming");
+  }
+}
+
+function generateBadge(task) {
+  let toReturn = "";
+  if (task.isCompleted === true) {
+    toReturn = `
+      <span class="text-xs px-2 bg-green-800 text-white py-1 rounded-full font-medium border ml-2">
+        Completed
+      </span>
+    `;
+  }
+  if (getDueLabel(task.due) === "Overdue") {
+    toReturn = `
+      <span class="text-xs px-2 bg-red-800 text-white py-1 rounded-full font-medium border ml-2">
+        Failed
+      </span>
+    `;
+  }
+
+  return toReturn;
 }
 
 function getDueLabel(dueDateStr) {
