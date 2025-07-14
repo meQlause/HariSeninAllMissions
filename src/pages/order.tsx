8884916827f356f1adbd0c9afe35ec0c40325407
meQlause <me@ardial.tech>
2025-07-14 16:44:11 +0700

    feat: enhance order page with category selection and responsive design improvements

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -2,15 +2,21 @@ import { TextInput } from "../components/UIs/input";
 import { DefaultLayout } from "../layouts/default";
 import { FooterLayout } from "../layouts/footer";
 import { HeaderLayout } from "../layouts/header";
-import { ShowProductComponent } from "../components/showProduct";
-import { PaginationUI } from "../components/UIs/pagination";
 import { getData } from "../services/api/getData";
 import type { Content } from "../utils/types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DividerUI } from "../components/UIs/divider";
+import { useIsMobile } from "../services/hooks/useIsMobile";
+import { mousePointerTracking, touchTracking } from "../utils/funtions";
 
 export const OrderPage = () => {
   const { contents } = getData();
+  const isMobile = useIsMobile();
+  const ulRef = useRef<HTMLUListElement>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string>("Semua Pesanan");
+  const selectCategory = (selected: string) => {
+    setSelectedCategory(selected);
+  };
   const [data, setData] = useState<Content[]>([]);
 
   useEffect(() => {
@@ -24,7 +30,7 @@ export const OrderPage = () => {
   return (
     <>
       <HeaderLayout />
-      <div className="px-standard flex flex-col gap-5 py-14 md:flex-row">
+      <div className="px-standard flex flex-col gap-5 py-14 lg:flex-row">
         <div className="flex w-full flex-col gap-4 md:max-w-64">
           <div className="flex flex-col gap-1">
             <h6 className="text-heading6 font-bold">Daftar Pesanan</h6>
@@ -49,24 +55,49 @@ export const OrderPage = () => {
         </div>
         <DefaultLayout className="w-full rounded-lg p-5">
           <div className="flex w-full flex-col">
-            <div className="flex flex-row justify-between gap-3" aria-disabled>
+            <div
+              className="flex flex-col items-center justify-between gap-3 lg:flex-row"
+              aria-disabled
+            >
               <div className="flex w-full max-w-[425px] flex-row justify-between gap-2">
-                <div className="flex flex-col items-start justify-start gap-2">
-                  <button className="text-left text-[#F64920]">Semua Pesanan</button>
-                  <DividerUI width="30px" color="#dc2626" thick="5px" />
-                </div>
-                <div className="flex flex-col items-start justify-start gap-2">
-                  <button className="text-left text-[#333333AD]">Menunggu</button>
-                  <DividerUI isHide={true} width="30px" color="#dc2626" thick="5px" />
-                </div>
-                <div className="flex flex-col items-start justify-start gap-2">
-                  <button className="text-left text-[#333333AD]">Berhasil</button>
-                  <DividerUI isHide={true} width="30px" color="#dc2626" thick="5px" />
-                </div>
-                <div className="flex flex-col items-start justify-start gap-2">
-                  <button className="text-left text-[#333333AD]">Gagal</button>
-                  <DividerUI isHide={true} width="30px" color="#dc2626" thick="5px" />
-                </div>
+                <ul
+                  className="scrollbar-hide mt-5 flex touch-pan-x flex-row gap-10 overflow-x-auto overflow-y-hidden"
+                  style={{ WebkitOverflowScrolling: "touch" }}
+                  ref={ulRef}
+                  onMouseDown={(e) => {
+                    if (ulRef.current) {
+                      mousePointerTracking(e.nativeEvent, ulRef.current);
+                    }
+                  }}
+                  onTouchStart={(e) => {
+                    if (ulRef.current) {
+                      touchTracking(e.nativeEvent, ulRef.current);
+                    }
+                  }}
+                >
+                  {["Semua Pesanan", "Menunggu", "Berhasil", "Gagal"].map((category) => (
+                    <li
+                      key={category}
+                      className={`cursor-pointer whitespace-nowrap font-sans font-medium hover:text-red-600 ${selectedCategory ? category === selectedCategory && "text-red-600" : category === "Semua Kelas" && "text-red-600"} flex flex-col gap-3`}
+                      onClick={() => selectCategory(category)}
+                    >
+                      {category}
+                      <div
+                        className={
+                          selectedCategory
+                            ? category === selectedCategory
+                              ? "block"
+                              : "hidden"
+                            : category === "Semua Kelas"
+                              ? "block"
+                              : "hidden"
+                        }
+                      >
+                        <DividerUI width="30px" color="#dc2626" thick="5px" />
+                      </div>
+                    </li>
+                  ))}
+                </ul>
               </div>
               <div className="flex flex-row gap-3">
                 <div className="relative">
@@ -85,33 +116,41 @@ export const OrderPage = () => {
             </div>
           </div>
           <div className="my-4 flex flex-col rounded-lg border">
-            <div className="flex flex-row items-center justify-between rounded-t-lg border-b bg-[#E2FCD933] px-5 py-3">
-              <div className="flex flex-row gap-2">
-                <span>
-                  NO. Invoice: <span>HEL/VI/10062023</span>
-                </span>
-                <span>Waktu Pembayaran: 10 Juni 2023, 14.17</span>
+            <div className="flex flex-col items-start justify-start gap-2 rounded-t-lg border-b bg-[#E2FCD933] px-5 py-3 lg:flex-row lg:items-center lg:justify-between">
+              <div className="flex flex-row items-center gap-2">
+                <span className="hidden lg:block lg:text-bodyMedium">NO. Invoice:</span>
+                <span className="text-bodySmall lg:text-bodyMedium">HEL/VI/10062023</span>
+                <span className="hidden lg:block lg:text-bodyMedium">Waktu Pembayaran: </span>
+                <span className="text-bodySmall lg:text-bodyMedium">10 Juni 2023, 14.17</span>
               </div>
-              <div className="rounded-md bg-[#E0FDDF] px-2 text-[#38D189]">Berhasil</div>
+              <div className="rounded-md bg-[#E0FDDF] px-2 text-bodySmall text-[#38D189]">
+                Berhasil
+              </div>
             </div>
-            <div className="flex flex-row items-center justify-between gap-5 p-5">
+            <div className="flex flex-col items-start justify-between gap-2 p-5 lg:flex-row lg:items-center lg:gap-5">
               <div className="flex flex-row items-center gap-3">
                 <img src="/assets/contents/content1.jpg" className="h-14 w-14 rounded-lg" />
-                <h6 className="text-heading6 font-bold">
+                <h6 className="text-bodyMedium font-bold lg:text-heading6">
                   Belajar Microsoft Office dan Google Workspace untuk Pemula
                 </h6>
               </div>
               <div className="flex flex-row gap-5">
-                <DividerUI orientation="vertical" width="50px" color="#3A35411F" thick="1px" />
+                <DividerUI
+                  isHide={!isMobile}
+                  orientation="vertical"
+                  width="50px"
+                  color="#3A35411F"
+                  thick="1px"
+                />
                 <div className="flex flex-col items-start">
                   <span>Harga</span>
-                  <span>Rp 300.000</span>
+                  <span className="font-bold">Rp 300.000</span>
                 </div>
               </div>
             </div>
             <div className="flex flex-row justify-between rounded-b-lg border-t bg-[#E2FCD933] px-5 py-3">
               <span>Total Pembayaran</span>
-              <span className="text-[#3ECF4C]">Rp. 300.000</span>
+              <span className="text-[#3ECF4C]">Rp 300.000</span>
             </div>
           </div>
         </DefaultLayout>
